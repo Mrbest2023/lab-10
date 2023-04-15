190481354bbc9104f49a891576c16de617c60266
Mrbest2023 <btbest@usc.edu>
2023-04-14 22:01:15 -0700

    45

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def final_process(data1: List[int], data2: List[int]) -> List[int]:
     """TODO: Document this function. What does it do? What are the inputs and outputs?"""
     return np.mean([x - y for x, y in zip(data1, data2)])
 
-offload_url = 'http://172.20.10.1:5000' # TODO: Change this to the IP address of your server
+offload_url = 'http://127.0.0.1:5000' # TODO: Change this to the IP address of your server
 
 def run(offload: Optional[str] = None) -> float:
     """Run the program, offloading the specified function(s) to the server.
@@ -55,12 +55,13 @@ def run(offload: Optional[str] = None) -> float:
         def offload_process1(data):
             nonlocal data1
             # TODO: Send a POST request to the server with the input data
-            r=requests.post("http://offload_url/process1", data={'foo':'bar'})
+            response=requests.post(f"{offload_url}/process1", json=data)
             data1 = response.json()
         thread = threading.Thread(target=offload_process1, args=(data,))
         thread.start()
         data2 = process2(data)
         thread.join()
+        
         # Question 2: Why do we need to join the thread here?
         # Question 3: Are the processing functions executing in parallel or just concurrently? What is the difference?
         #   See this article: https://oxylabs.io/blog/concurrency-vs-parallelism
@@ -71,13 +72,13 @@ def run(offload: Optional[str] = None) -> float:
         data2=None
         def offload_process2(data):
             nonlocal data2
-            r_2=requests.post("http://offload_url/process2", data={'foo':'bar'})
+            r_2=requests.post("http://offload_url/process2", json=data)
             data2=response.json()
         thread = threading.Thread(target=offload_process2, args=(data,))
         thread.start()
         data1=process1(data)
         thread.join()
-        pass
+        
 
     elif offload == 'both':
         # TODO: Implement this case
@@ -85,22 +86,20 @@ def run(offload: Optional[str] = None) -> float:
         def offload_process1(data):
             nonlocal data1
             # TODO: Send a POST request to the server with the input data
-            r=requests.post("http://offload_url/process1", data={'foo':'bar'})
+            r=requests.post("http://offload_url/process1", json=data)
             data1 = response.json()
         data2=None
         def offload_process2(data):
             nonlocal data2
-            r_2=requests.post("http://offload_url/process2", data={'foo':'bar'})
+            r_2=requests.post("http://offload_url/process2", json=data)
             data2=response.json()
         thread1=threading.Thread(target=offload_process1, args=(data,))
-        thread1.start()
-        data2=process2(data)
-        thread1.join()
         thread2=threading.Thread(target=offload_process2, args=(data,))
+        thread1.start()
         thread2.start()
-        data1=process1(data)
+        thread1.join()
         thread2.join()
-        pass
+        
 
     ans = final_process(data1, data2)
     return ans 
@@ -131,9 +130,13 @@ def main():
     # Make sure to include a title and x and y labels
     
     plt.bar(modes,df3)   
+    plt.title('lab 10 plot')
+    plt.xlabel('modes')
+    plt.ylabel('times')
 
     # TODO: save plot to "makespan.png"
     plt.savefig('makespan.png"')
+
 
     # Question 4: What is the best offloading mode? Why do you think that is?
     # Question 5: What is the worst offloading mode? Why do you think that is?
--- server.py
+++ server.py
@@ -9,13 +9,15 @@ app = Flask(__name__)
 def index():
     return jsonify({'message': 'Welcome'})
 @app.route('/process1')
-def process1()
+def pro1():
     data= request.get_json()
-return jsonify(process1(data))
+    return jsonify(process1(data))
 @app.route('/process2')
-def process2()
+def pro2():
     data=request.get_json()
-return jsonify(process2(data))
+    return jsonify(process2(data))
+if __name__ == '__main__':
+    app.run("0.0.0.0")
 
 # TODO: Create a flask app with two routes, one for each function.
 # The route should get the data from the request, call the function, and return the result.
